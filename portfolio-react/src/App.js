104d322418287a05596d3b73629454530e8487aa
AlexFloresVeliz <alexfloresveliz@outlook.com>
2018-04-16 00:49:46 -0400

    Add navbar

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class NavBar extends Component {
+  render() {
+    return (
+      <nav>
+        <ul className="nav">
+          <li><a className="navItem" href="#">Link</a></li>
+          <li><a className="navItem" href="#">Link</a></li>
+          <li><a className="navItem" href="#">Link</a></li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -8,6 +22,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Alex Flores-Veliz</h1>
           <p className="App-intro">Hello World</p>
+          <NavBar />
         </header>
       </div>
     );
