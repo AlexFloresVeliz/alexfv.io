773ac45800814116b223f80cfcc54d8617d5efa2
AlexFloresVeliz <alexfloresveliz@outlook.com>
2018-04-17 04:41:41 -0400

    updates

---
+++ README.md
@@ -0,0 +1,23 @@
+# Portfolio Website :globe_with_meridians:
+
+It is becoming more and more important to create a personal brand in order to build a reputation for your specific talents and services. I decided to create a website to promote what skills I can offer a team or business.
+
+> I'm not a businessman; I'm a business, man! - Jay-Z 🧐👔
+
+## Built With 🎨🛠️
+
+My online portfolio was created in [React](https://reactjs.org/)! ⚛
+
+## Preview 👀
+
+### *Homepage*
+![www alexfv io_](https://user-images.githubusercontent.com/23161228/38596241-617ec15a-3d1e-11e8-84ec-fae5a6f77bf7.png)
+
+### *Desktop*
+<img src="https://user-images.githubusercontent.com/23161228/38764045-cbde6614-3f75-11e8-8147-9bc4cf065cc4.png">
+
+## *iPhone*
+<img src="https://user-images.githubusercontent.com/23161228/38764091-707da9aa-3f76-11e8-99d9-3227f80dfd03.png" width="40%" height="40%">
+
+## *iPad*
+<img src="https://user-images.githubusercontent.com/23161228/38764072-1d48b018-3f76-11e8-9276-c0c58180e654.png" width="50%" height="50%">
---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+import NavBar from './NavBar'
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <div className="footer">
+          <a href="https://www.linkedin.com/in/alexfloresveliz/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin" aria-hidden="true" id="nav-link"></i></a>
+          <a href="https://github.com/AlexFloresVeliz" target="_blank" rel="noopener noreferrer"><i class="fab fa-github-alt" aria-hidden="true" id="nav-link"></i></a>
+          <a href="https://www.behance.net/alexfloresveliz" target="_blank" rel="noopener noreferrer"><i class="fab fa-behance" aria-hidden="true" id="nav-link"></i></a>
+          <a href="mailto:hi@alexfv.io"><i class="far fa-envelope" aria-hidden="true" id="nav-link"></i></a>
+        </div>
+      </footer>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Alex F-V</h1>
+          <NavBar />
+          <Footer />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
